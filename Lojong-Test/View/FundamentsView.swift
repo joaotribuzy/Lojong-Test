48f5f56b481bef0100e16968093e716f8a536265
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2020-03-27 14:26:53 -0400

    Days views positions setting

--- Lojong-Test/View/FundamentsView.swift
+++ Lojong-Test/View/FundamentsView.swift
@@ -72,7 +72,7 @@ extension FundamentsView: LojongCustomView{
             
             let dayLabel: UILabel = UILabel()
             dayLabel.text = "Dia \(current + 1)"
-            dayLabel.font = UIFont(name: "Asap-Regular", size: 11)
+            dayLabel.font = UIFont(name: "Asap-Medium", size: 11)
             
             view.sv(dayLabel)
             view.contentMode = .scaleAspectFit
@@ -84,6 +84,12 @@ extension FundamentsView: LojongCustomView{
             case 2,4,6,10:
                 view.image = UIImage(named: "horizontal-ground")
                 dayLabel.textColor = UIColor.init(red: 139, green: 87, blue: 41)
+            case 12,13,14,15,16,18,19,20,21,22,23,25,26,27,29,30:
+                view.image = UIImage(named: "vertical-water")
+                dayLabel.textColor = .white
+            case 17,24,28:
+                view.image = UIImage(named: "horizontal-water")
+                dayLabel.textColor = .white
             default:
                 break
             }
@@ -123,15 +129,24 @@ extension FundamentsView: LojongCustomView{
         for current in 0...30{
         
             imageBackGround.sv(listViewClick[current])
-            listViewClick[current].subviews[0].centerHorizontally().bottom(28)
             
             switch current {
-            case 0,1,3:
+            case 0,1,3,5,7,8,9,11:
                 listViewClick[current].width(widthPointsTransform(76))
                 listViewClick[current].height(heightPointsTranform(81))
-            case 2,4:
+                listViewClick[current].subviews[0].centerHorizontally().bottom(heightPointsTranform(23))
+            case 2,4,6,10:
                 listViewClick[current].width(widthPointsTransform(75))
-                listViewClick[current].height(heightPointsTranform(72.5))
+                listViewClick[current].height(heightPointsTranform(72))
+                listViewClick[current].subviews[0].centerHorizontally().bottom(heightPointsTranform(20))
+            case 12,13,14,15,16,18,19,20,21,22,23,25,26,27,29,30:
+                listViewClick[current].width(widthPointsTransform(85.5))
+                listViewClick[current].height(heightPointsTranform(85.5))
+                listViewClick[current].subviews[0].centerHorizontally().bottom(heightPointsTranform(23))
+            case 17,24,28:
+                listViewClick[current].width(widthPointsTransform(85))
+                listViewClick[current].height(heightPointsTranform(86.5))
+                listViewClick[current].subviews[0].centerHorizontally().bottom(heightPointsTranform(25))
             default:
                 break
             }
@@ -144,14 +159,92 @@ extension FundamentsView: LojongCustomView{
                 listViewClick[current].right(widthPointsTransform(77.3))
                 listViewClick[current].bottom(heightPointsTranform(161))
             case 2:
-                listViewClick[current].width(90).height(79.5).right(185)
-                listViewClick[current].Bottom == 280
+                listViewClick[current].right(widthPointsTransform(167))
+                listViewClick[current].bottom(heightPointsTranform(253.5))
             case 3:
-                listViewClick[current].width(84.3).height(90).left(40)
-                listViewClick[current].Bottom == 367
+                listViewClick[current].left(widthPointsTransform(36.5))
+                listViewClick[current].bottom(heightPointsTranform(332))
             case 4:
-                listViewClick[current].width(90).height(79.5).centerHorizontally()
-                listViewClick[current].Bottom == 472
+                listViewClick[current].centerHorizontally()
+                listViewClick[current].bottom(heightPointsTranform(427.5))
+            case 5:
+                listViewClick[current].right(widthPointsTransform(31))
+                listViewClick[current].bottom(heightPointsTranform(508.5))
+            case 6:
+                listViewClick[current].right(widthPointsTransform(120))
+                listViewClick[current].bottom(heightPointsTranform(605.5))
+            case 7:
+                listViewClick[current].right(widthPointsTransform(212))
+                listViewClick[current].bottom(heightPointsTranform(697.5))
+            case 8:
+                listViewClick[current].right(widthPointsTransform(212))
+                listViewClick[current].bottom(heightPointsTranform(798))
+            case 9:
+                listViewClick[current].right(widthPointsTransform(212))
+                listViewClick[current].bottom(heightPointsTranform(898))
+            case 10:
+                listViewClick[current].right(widthPointsTransform(137))
+                listViewClick[current].bottom(heightPointsTranform(1009.5))
+            case 11:
+                listViewClick[current].right(widthPointsTransform(62))
+                listViewClick[current].bottom(heightPointsTranform(1113.5))
+            case 12:
+                listViewClick[current].right(widthPointsTransform(221))
+                listViewClick[current].bottom(heightPointsTranform(1420.5))
+            case 13:
+                listViewClick[current].right(widthPointsTransform(221))
+                listViewClick[current].bottom(heightPointsTranform(1544))
+            case 14:
+                listViewClick[current].right(widthPointsTransform(98.5))
+                listViewClick[current].bottom(heightPointsTranform(1720))
+            case 15:
+                listViewClick[current].left(widthPointsTransform(28.5))
+                listViewClick[current].bottom(heightPointsTranform(1886))
+            case 16:
+                listViewClick[current].left(widthPointsTransform(28.5))
+                listViewClick[current].bottom(heightPointsTranform(2009))
+            case 17:
+                listViewClick[current].right(widthPointsTransform(166))
+                listViewClick[current].bottom(heightPointsTranform(2106))
+            case 18:
+                listViewClick[current].right(widthPointsTransform(79.5))
+                listViewClick[current].bottom(heightPointsTranform(2190))
+            case 19:
+                listViewClick[current].right(widthPointsTransform(79.5))
+                listViewClick[current].bottom(heightPointsTranform(2288))
+            case 20:
+                listViewClick[current].right(widthPointsTransform(79.5))
+                listViewClick[current].bottom(heightPointsTranform(2388))
+            case 21:
+                listViewClick[current].right(widthPointsTransform(202))
+                listViewClick[current].bottom(heightPointsTranform(2550))
+            case 22:
+                listViewClick[current].right(widthPointsTransform(40))
+                listViewClick[current].bottom(heightPointsTranform(2705))
+            case 23:
+                listViewClick[current].right(widthPointsTransform(40))
+                listViewClick[current].bottom(heightPointsTranform(2834))
+            case 24:
+                listViewClick[current].right(widthPointsTransform(130))
+                listViewClick[current].bottom(heightPointsTranform(2931.5))
+            case 25:
+                listViewClick[current].left(widthPointsTransform(68.5))
+                listViewClick[current].bottom(heightPointsTranform(3024))
+            case 26:
+                listViewClick[current].left(widthPointsTransform(68.5))
+                listViewClick[current].bottom(heightPointsTranform(3140))
+            case 27:
+                listViewClick[current].left(widthPointsTransform(68.5))
+                listViewClick[current].bottom(heightPointsTranform(3262))
+            case 28:
+                listViewClick[current].right(widthPointsTransform(134))
+                listViewClick[current].bottom(heightPointsTranform(3359))
+            case 29:
+                listViewClick[current].right(widthPointsTransform(40))
+                listViewClick[current].bottom(heightPointsTranform(3467))
+            case 30:
+                listViewClick[current].right(widthPointsTransform(40))
+                listViewClick[current].bottom(heightPointsTranform(3589))
             default:
                 break
             }
